f8788c3dc1ad6f2815a82268dc706d1a27ab861b
letitiacowan <letitia.cowan01@hotmail.com>
2025-12-12 17:30:16 +1100

    prod issue fix

--- app/logged-in/for-you/components/BooksBar.tsx
+++ app/logged-in/for-you/components/BooksBar.tsx
@@ -1,7 +1,12 @@
 import React from "react";
 import BookCard from "@/app/components/BookCard";
+import { Book } from "@/app/types/book";
 
-const BooksBar = ({ books }) => {
+interface BooksBarProps {
+  books: Book[];
+}
+
+const BooksBar: React.FC<BooksBarProps> = ({ books }) => {
   return (
     <div className="overflow-x-auto lg:overflow-visible -mx-4 px-4 lg:mx-0 lg:px-0">
       <div className="flex flex-row gap-2 sm:gap-3 md:gap-4 lg:flex-wrap lg:min-w-0">
